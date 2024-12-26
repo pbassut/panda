016de231e82d55549cc25933da17a75a35f290ec
Patrick Bassut <patrickbassut@hotmail.com>
2024-12-26 19:01:29 -0300

    bcm signal

--- board/safety/safety_fiat.h
+++ board/safety/safety_fiat.h
@@ -3,6 +3,7 @@
 #include "safety_declarations.h"
 
 typedef struct {
+  const int BCM_2;
   const int ABS_6;
   const int DAS_1;
   const int DAS_2;
@@ -155,6 +156,7 @@ static int fiat_fwd_hook(int bus_num, int addr) {
 }
 
 const FiatAddrs FASTBACK_ADDRS = {
+  .BCM_2            = 0x73E,
   .ABS_6            = 0x101,
   .DAS_1            = 0x2FA,
   .DAS_2            = 0x5A5,
@@ -167,6 +169,7 @@ static safety_config fiat_init(uint16_t param) {
   gen_crc_lookup_table_8(0x1D, fca_fastback_crc8_lut_j1850);
 
   static RxCheck fastback_rx_checks[] = {
+    {.msg = {{FASTBACK_ADDRS.BCM_2,         0, 4, .check_checksum = false,      .max_counter = 0U, .frequency = 4U}, { 0 }, { 0 }}},
     {.msg = {{FASTBACK_ADDRS.ABS_6,         0, 8, .check_checksum = true,      .max_counter = 15U, .frequency = 100U}, { 0 }, { 0 }}},
     {.msg = {{FASTBACK_ADDRS.DAS_1,         1, 4, .check_checksum = true,      .max_counter = 15U, .frequency = 50U},  { 0 }, { 0 }}},
     {.msg = {{FASTBACK_ADDRS.DAS_2,         1, 8, .check_checksum = false,     .max_counter = 0U,  .frequency = 1U},   { 0 }, { 0 }}},
